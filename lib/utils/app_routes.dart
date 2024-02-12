6f7171d8e08f71bc2c37eb1933a43531e488d922
Sanket Patel <44511437+s4nk37@users.noreply.github.com>
2024-02-12 23:51:15 +0530

    Removed unwanted code

--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -6,14 +6,8 @@ class AppRoutes {
   AppRoutes._();
 
   static const String splashScreen = '/';
-  // static const String homeScreen = '/home_screen';
-  // static const String questionScreen = '/question_screen';
-  // static const String quizEndScreen = '/quiz_end_screen';
 
   static Map<String, Widget Function(BuildContext)> routes = {
     "/": (context) => const HomeScreen(),
-    // Routes.homeScreen: (context) => const HomeScreen(),
-    // Routes.questionScreen: (context) => const QuestionScreen(),
-    //Routes.quizEndScreen: (context) => const QuizEndScreen(),
   };
 }
